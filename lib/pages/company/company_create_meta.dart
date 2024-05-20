fc48fd85a9b10a427d607f97536ec5f13647b947
DevBruno <brunoantonio.dev0016@gmail.com>
2024-05-20 12:28:30 -0300

    Criação da página de criação de metas

---
+++ lib/pages/company/company_create_meta.dart
@@ -0,0 +1,126 @@
+import 'package:enercicio/pages/company/company_home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CompanyCreateMeta extends StatefulWidget {
+  const CompanyCreateMeta({super.key});
+
+  @override
+  State<CompanyCreateMeta> createState() => _CompanyCreateMetaState();
+}
+
+class _CompanyCreateMetaState extends State<CompanyCreateMeta> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Criar meta',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(
+            color: Colors.grey.shade900,
+            height: 1.0,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    width: 400,
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.shade900),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(10.0)
+                      )
+                    ),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                            labelText: "Objetivo",
+                            labelStyle: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20,
+                            ),
+                          ),
+                          style: const TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                        const SizedBox(height: 30,),
+                        TextFormField(
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                            labelText: "Recompensa",
+                            labelStyle: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 20,
+                            ),
+                          ),
+                          style: const TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                        const SizedBox(height: 30,),
+                        Container(
+                          width: 150,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                              colors: [
+                                Colors.pink,
+                                Colors.purple,
+                              ]
+                            ),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: TextButton(
+                            onPressed: () => {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const CompanyHomePage()),
+                              ),
+                            },
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                            ),
+                            child: const Text(
+                              "Criar meta",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),    
+          ],
+        ),
+      ),
+    );
+  }
+}
