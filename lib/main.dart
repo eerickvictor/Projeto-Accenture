b6cc928ad49d28bbb53eb29d880eaf3016d98eb6
DevBruno <brunoantonio.dev0016@gmail.com>
2024-05-25 21:17:31 -0300

    Adição das rotas nomeadas que referenciam os cadastros de empresa e atletas

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:enercicio/pages/athlete/athlete_edit_perfil_page.dart';
 import 'package:enercicio/pages/athlete/athlete_home_page.dart';
 import 'package:enercicio/pages/athlete/athlete_perfil_page.dart';
 import 'package:enercicio/pages/athlete/pageLoginAtleta.dart';
+import 'package:enercicio/pages/company/CompanyRegisterUser.dart';
 import 'package:enercicio/pages/company/PageCadastroCompany.dart';
 import 'package:enercicio/pages/company/company_create_meta.dart';
 import 'package:enercicio/pages/company/company_edit_perfil_page.dart';
@@ -44,6 +45,7 @@ class MyApp extends StatelessWidget {
         '/cadastro_empresa': (context) => const PageCadastroCompany(),
         '/home_empresa': (context) => const CompanyHomePage(),
         '/criar_meta_empresa': (context) => const CompanyCreateMeta(),
+        '/cadastrar_usuario_meta': (context) => const CompanyRegisterUser(),
         '/editar_meta_empresa': (context) => const CompanyUpadateMeta(),
         '/lista_usuarios_empresa': (context) => const CompanyUsersList(),
         '/perfil_empresa': (context) => const CompanyPerfilPage(),
