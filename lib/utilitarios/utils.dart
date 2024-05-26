0eb3211c1073f8cf0623e168816d682ce99ccc27
DevBruno <brunoantonio.dev0016@gmail.com>
2024-05-25 21:19:56 -0300

    Criação do component useContainer que vai referenciar os blocos existentes na lista de usuarios presente na tela de empresa

--- lib/utilitarios/utils.dart
+++ lib/utilitarios/utils.dart
@@ -21,6 +21,7 @@ Widget buildTextField(BuildContext context, String labelText,
         labelText: labelText,
         labelStyle: const TextStyle(
           color: Colors.white,
+          fontSize: 20,
         ),
         floatingLabelBehavior: FloatingLabelBehavior.always,
         hintText: placeholder,
@@ -81,3 +82,99 @@ Widget textButton(
     ),
   );
 }
+
+Widget userContainer(String userName, String qtdWatts) {
+  return Container(
+    decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade900),
+        borderRadius: const BorderRadius.all(Radius.circular(5.0))),
+    child: Row(
+      children: [
+        const Icon(
+          Icons.person,
+          color: Colors.purple,
+          size: 50,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              userName,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'QuiloWatts Gerados: $qtdWatts',
+              style: const TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Widget spacing(double width, double height) {
+  return SizedBox(
+    width: width,
+    height: height,
+  );
+}
+
+// Widget perfilContainer(BuildContext context, String iconString,String title, String tagButton, String router)
+// {
+  
+//   final IconData iconData = IconData(int.parse(iconString));
+
+//   return Container(
+//               padding: const EdgeInsets.all(10.0),
+//               decoration: BoxDecoration(
+//                 border: Border.all(color: Colors.grey.shade900),
+//                 borderRadius: const BorderRadius.all(
+//                   Radius.circular(5.0),
+//                 ),
+//               ),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Row(
+//                     children: [
+//                       const Icon(
+//                         Icons.edit,
+//                         color: Colors.white,
+//                       ),
+//                       spacing(10, 0),
+//                       const Text(
+//                         'Editar perfil',
+//                         style: TextStyle(
+//                           fontSize: 20,
+//                           fontWeight: FontWeight.w500,
+//                           color: Colors.white,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                   FloatingActionButton.small(
+//                     heroTag: 'btnEdit',
+//                     onPressed: () => {
+//                       Navigator.of(context).pushNamed('/editar_perfil_empresa'),
+//                     },
+//                     backgroundColor: Colors.purple,
+//                     shape: const CircleBorder(),
+//                     child: const Icon(
+//                       Icons.arrow_forward_outlined,
+//                       color: Colors.white,
+//                     ),
+//                   )
+//                 ],
+//               ),
+//             );
+// }
+
