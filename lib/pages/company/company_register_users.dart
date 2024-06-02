9a1192c01c3768250cfe055f09cf7096a97dfba0
DevBruno <brunoantonio.dev0016@gmail.com>
2024-06-02 14:20:23 -0300

    Renomeção do nome da pagina CompanyRegisterUser para company_register_users

---
+++ lib/pages/company/company_register_users.dart
@@ -0,0 +1,133 @@
+import 'package:enercicio/utilitarios/utils.dart';
+import 'package:flutter/material.dart';
+
+class CompanyRegisterUser extends StatefulWidget {
+  const CompanyRegisterUser({super.key});
+
+  @override
+  State<CompanyRegisterUser> createState() => _CompanyRegisterUserState();
+}
+
+class _CompanyRegisterUserState extends State<CompanyRegisterUser> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Cadastrar Usuario',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(
+            color: Colors.grey.shade900,
+            height: 1.0,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    width: 400,
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey.shade900),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10.0))),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: getAuthenticationInputDecoration("CPF", false, false, ""),
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
+                            validator: (String? value) {
+                              if(value == "") {
+                                return "O campo que recebe o CPF não pode ser vazio";
+                              }
+                              return null;
+                            },
+                          ),
+                          spacing(0, 20),
+                          TextFormField(
+                            decoration: getAuthenticationInputDecoration("E-mail", false, false, ""),
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
+                            validator: (String? value) {
+                              if(value == "") {
+                                return "O campo que recebe o E-mail não pode ser vazio";
+                              }
+                              if(value!.length < 5) {
+                                return "O e-mail informado é muito curto";
+                              }
+                              if(!value.contains("@")) {
+                                return "O e-mail informado não é valido";
+                              }
+                              return null;
+                            },
+                          ),
+                          spacing(0, 30),
+                          Container(
+                            width: 150,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(colors: [
+                                Colors.pink,
+                                Color.fromRGBO(156, 39, 176, 1),
+                              ]),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: TextButton(
+                              onPressed: () {
+                                  botaoCadastrar();
+                              },
+                              child: const Text(
+                                "Cadastrar",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          spacing(0, 10),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  botaoCadastrar() {
+    if (_formKey.currentState!.validate()) {
+      print("Formulario valido");
+      Navigator.of(context).pop();
+    } else{
+      
+      print("Formulario invalido");
+    }
+  }
+}
