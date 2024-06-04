3fd6fb81ef934250f35b6aa4b6fe48d391b7d43d
DevBruno <brunoantonio.dev0016@gmail.com>
2024-06-04 10:14:28 -0300

    Adição de novas rotas. Pagina de exercicios empresa e gerenciamento de metas

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:enercicio/pages/athlete/PageCadastroAtleta.dart';
+import 'package:enercicio/pages/athlete/athlete_company_exercicios.dart';
 import 'package:enercicio/pages/athlete/athlete_company_page.dart';
 import 'package:enercicio/pages/athlete/athlete_edit_perfil_page.dart';
 import 'package:enercicio/pages/athlete/athlete_exercicio.dart';
 import 'package:enercicio/pages/athlete/athlete_home_page.dart';
 import 'package:enercicio/pages/athlete/athlete_perfil_page.dart';
 import 'package:enercicio/pages/athlete/pageLoginAtleta.dart';
-import 'package:enercicio/pages/company/CompanyRegisterUser.dart';
 import 'package:enercicio/pages/company/PageCadastroCompany.dart';
 import 'package:enercicio/pages/company/company_create_meta.dart';
 import 'package:enercicio/pages/company/company_edit_perfil_page.dart';
 import 'package:enercicio/pages/company/company_home_page.dart';
+import 'package:enercicio/pages/company/company_manage_meta.dart';
 import 'package:enercicio/pages/company/company_perfil_page.dart';
+import 'package:enercicio/pages/company/company_register_users.dart';
 import 'package:enercicio/pages/company/company_update_meta.dart';
 import 'package:enercicio/pages/company/company_users_list.dart';
 import 'package:enercicio/pages/company/pageLoginCompany.dart';
@@ -41,16 +43,18 @@ class MyApp extends StatelessWidget {
         '/home_atleta': (context) => const AthleteHomePage(),
         '/home_atleta_empresa': (context) => const AthleteCompanyPage(),
         '/perfil_atleta': (context) => const AthletePerfilPage(),
+        '/exercicio_empresa_atleta': (context) => const AthleteCompanyExercicios(),
         '/atleta_exercicios': (context) => const AthleteExercicio(),
         '/editar_perfil_atleta': (context) => const AthleteEditPerfilPage(),
         '/login_empresa': (context) => const PageLoginCompany(),
         '/cadastro_empresa': (context) => const PageCadastroCompany(),
         '/home_empresa': (context) => const CompanyHomePage(),
+        '/gerenciar_meta_empresa': (context) => const CompanyManageMeta(),
         '/criar_meta_empresa': (context) => const CompanyCreateMeta(),
         '/cadastrar_usuario_meta': (context) => const CompanyRegisterUser(),
         '/editar_meta_empresa': (context) => const CompanyUpadateMeta(),
         '/lista_usuarios_empresa': (context) => const CompanyUsersList(),
-        '/perfil_empresa': (context) => const CompanyPerfilPage(),
+        '/perfil_empresa': (context) => const CompanyPerfilPage(),        
         '/editar_perfil_empresa': (context) => const CompanyEditPerfilPage(),
       },
     );
