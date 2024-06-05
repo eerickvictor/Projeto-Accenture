552c2aebdae9f157f084913d715327af3fff592a
DevBruno <brunoantonio.dev0016@gmail.com>
2024-06-05 00:46:29 -0300

    Criação do metodo getAllCompanys que vai pegar todas as empresas do banco e adição desses valores na estrutura de widgets

--- lib/pages/athlete/athlete_home_page.dart
+++ lib/pages/athlete/athlete_home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
 class AthleteHomePage extends StatefulWidget {
@@ -12,28 +12,16 @@ class AthleteHomePage extends StatefulWidget {
 }
 
 class _AthleteHomePageState extends State<AthleteHomePage> {
-
   String? athleteId;
-
-  ParseObject? company;
-
-  void pressed() {
-    print('pressionado');
-  }
 
   @override
   Widget build(BuildContext context) {
-
     final Map arguments = ModalRoute.of(context)?.settings.arguments as Map;
-    print(athleteId);
     if (arguments != null) {
       setState(() {
         athleteId = arguments['athleteId'];
-        print(athleteId);
       });
     }
-
-    getCompany();
 
     return Scaffold(
       backgroundColor: Colors.black,
@@ -57,55 +45,74 @@ class _AthleteHomePageState extends State<AthleteHomePage> {
         width: 400,
         padding: const EdgeInsets.all(10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  Container(
-                    child: Column(
-                      children: [
-                         const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'Empresas',
-                            // athleteId,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(10.0),
-                          width: 400,
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey.shade900),
-                              borderRadius: const BorderRadius.all(
-                                  Radius.circular(10.0))),
-                          child: Column(
-                            children: [
-                              companyContainer("Academia da Cidade",
-                                  "assets/images/stations/station_imagem.png"),
-                              companyContainer("Shopping Recife",
-                                  "assets/images/stations/station_imagem.png"),
-                              companyContainer("Academia BodyBuilder",
-                                  "assets/images/stations/station_imagem.png"),
-                              companyContainer("Porto Digital",
-                                  "assets/images/stations/station_imagem.png"),
-                              companyContainer("Posto Machado98",
-                                  "assets/images/stations/station_imagem.png"),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+            const Text(
+              'Empresas',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
               ),
             ),
+            // Container(
+            //   padding: const EdgeInsets.all(10.0),
+            //   width: 400,
+            //   decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.grey.shade900),
+            //     borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            //   ),
+            //   child: Column(
+            //     children: [
+                  //Este widget permite que as operações realizadas dentro dele possam ser realizadas e utilizadas de maneira assincrona
+                  FutureBuilder<List<ParseObject>>(
+                    //Recebimento de dados de maneira assincrona
+                    future: getAllCompany(),
+                    //Passagem de um contexto atual e um contexto futuro com snapshots, que seriam prints de estado do valor que foi passado
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (snapshot.hasError) {
+                        return const Center(
+                          child: Text("Error.."),
+                        );
+                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return const Center(
+                          child: Text("No data"),
+                        );
+                      } else {
+                        return Container(
+                          padding: const EdgeInsets.all(10.0),
+                          width: 400,
+                          height: 630,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey.shade900),
+                            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                          ),
+                          //É uma lista de objetos 
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: snapshot.data!.length,
+                            //Função que vai pegar a quantidade de itens definidos no itemCount e realizar algo referente a essa quantidade
+                            itemBuilder: (context, index) {
+                              final company = snapshot.data![index];
+                              final companyName = company.get<String>('nome');
+                              return companyContainer(
+                                companyName!,
+                                "assets/images/stations/station_imagem.png",
+                              );
+                            },
+                          ),
+                        );
+                      }
+                    },
+                  ),
+            //     ],
+            //   ),
+            // ),
           ],
         ),
       ),
@@ -113,9 +120,11 @@ class _AthleteHomePageState extends State<AthleteHomePage> {
         padding: const EdgeInsets.only(),
         height: 60,
         decoration: BoxDecoration(
-            color: Colors.transparent,
-            border: Border(
-                top: BorderSide(color: Colors.grey.shade900, width: 1.0))),
+          color: Colors.transparent,
+          border: Border(
+            top: BorderSide(color: Colors.grey.shade900, width: 1.0),
+          ),
+        ),
         child: SizedBox(
           height: 20,
           width: 500,
@@ -139,55 +148,56 @@ class _AthleteHomePageState extends State<AthleteHomePage> {
   }
 
   Widget companyContainer(String companyName, String urlCompanyImage) {
-    return Column(children: [
-      SizedBox(
-        width: 250,
-        height: 250,
-        child: ElevatedButton(
-          onPressed: () => {
-            Navigator.of(context).pushNamed('/home_atleta_empresa'),
-          },
-          style: ElevatedButton.styleFrom(
+    return Column(
+      children: [
+        SizedBox(
+          width: 250,
+          height: 250,
+          child: ElevatedButton(
+            onPressed: () => {
+              Navigator.of(context).pushNamed('/home_atleta_empresa'),
+            },
+            style: ElevatedButton.styleFrom(
               backgroundColor: Colors.transparent,
               shadowColor: Colors.transparent,
-              shape: const BeveledRectangleBorder()),
-          child: Column(
-            children: [
-              SizedBox(
-                width: 250,
-                height: 200,
-                child: Image.asset(urlCompanyImage),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  companyName,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15,
+              shape: const BeveledRectangleBorder(),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 250,
+                  height: 200,
+                  child: Image.asset(urlCompanyImage),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    companyName,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      )
-    ]);
+      ],
+    );
   }
-}
 
+  Future<List<ParseObject>> getAllCompany() async {
+    QueryBuilder<ParseObject> company = QueryBuilder<ParseObject>(ParseObject('Empresa'));
+    company.keysToReturn(['nome', 'objectId']);
+    final ParseResponse apiRespose = await company.query();
 
-Future<List<ParseObject>> getCompany() async {
-  QueryBuilder<ParseObject> queryGetCompany = QueryBuilder<ParseObject>(ParseObject('Empresa'));
-  queryGetCompany.keysToReturn(['nome', 'objectId']);
-  final ParseResponse apiRespose = await queryGetCompany.query();
-
-  if(apiRespose.success && apiRespose.results != null) {
-    print(apiRespose.results as List<ParseObject>);
-    return apiRespose.results as List<ParseObject>;
-  } else {
-    return [];
+    if (apiRespose.success && apiRespose.results != null) {
+      return apiRespose.results as List<ParseObject>;
+    } else {
+      return [];
+    }
   }
 }
